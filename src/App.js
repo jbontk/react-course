74e4a258541def97b4bfa017f0dbdab7cb12aad5
jean <jbontikous@yahoo.fr>
2021-12-01 18:32:56 +0100

    466

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
 
+import MainNavigation from './components/layout/MainNavigation';
+
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path='/' exact>
           <AllMeetupsPage />
