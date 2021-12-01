73a9fbe0d33329837975382f17f6dd49c0acbda0
jean <jbontikous@yahoo.fr>
2021-12-01 15:08:54 +0100

    458

---
+++ src/components/Todo.js
@@ -0,0 +1,10 @@
+const Todo = () => (
+  <div className="card">
+    <h2>TITLE</h2>
+    <div className="actions">
+      <button className="btn">Delete</button>
+    </div>
+  </div>
+);
+
+export default Todo;
