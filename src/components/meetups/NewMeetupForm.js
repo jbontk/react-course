0d6090426a5255fb97c3e3cfc60add56e68b2681
jean <jbontikous@yahoo.fr>
2021-12-02 18:30:54 +0100

    472

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,19 +1,61 @@
+import { useRef } from 'react';
+
 import Card from '../ui/Card';
 import Control from '../ui/Control';
 import classes from './NewMeetupForm.module.css';
 
-const NewMeetupForm = () => (
-  <Card>
-    <form className={classes.form}>
-      <Control id='title' label='Meetup Title' type='text' />
-      <Control id='image' label='Image' type='url' />
-      <Control id='address' label='Address' type='text' />
-      <Control id='description' label='Description' type='textarea' />
-      <div className={classes.actions}>
-        <button>Add Meetup</button>
-      </div>
-    </form>
-  </Card>
-);
+const NewMeetupForm = () => {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    console.log(meetupData);
+  };
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <Control
+          id='title'
+          label='Meetup Title'
+          type='text'
+          ref={titleInputRef}
+        />
+        <Control id='image' label='Image' type='url' ref={imageInputRef} />
+        <Control
+          id='address'
+          label='Address'
+          type='text'
+          ref={addressInputRef}
+        />
+        <Control
+          id='description'
+          label='Description'
+          type='textarea'
+          ref={descriptionInputRef}
+        />
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+};
 
 export default NewMeetupForm;
--- src/components/ui/Control.js
+++ src/components/ui/Control.js
@@ -1,15 +1,18 @@
+import React from 'react';
 import classes from './Control.module.css';
 
-const Control = (props) => (
+// https://reactjs.org/docs/forwarding-refs.html
+
+const Control = React.forwardRef((props, ref) => (
   <div className={classes.control}>
     <label htmlFor={props.id}>{props.label}</label>
     {props.type !== 'textarea' && (
-      <input type={props.type} id={props.id} required></input>
+      <input type={props.type} id={props.id} required ref={ref}></input>
     )}
     {props.type === 'textarea' && (
-      <textarea rows='5' id={props.id} required></textarea>
+      <textarea rows='5' id={props.id} required ref={ref}></textarea>
     )}
   </div>
-);
+));
 
 export default Control;
