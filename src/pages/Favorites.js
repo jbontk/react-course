ba183710ce6ccf7529b4d6ecacb8ccd0d725d0d1
jean <jbontikous@yahoo.fr>
2021-12-03 19:59:46 +0100

    480

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,24 @@
+import { useContext } from 'react';
+import FavoritesContext from '../store/favorites-context';
+
+import MeetupList from '../components/meetups/MeetupList';
+
 const FavoritesPage = () => {
-  return <div>Favorites Page</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content =
+    favoritesCtx.totalFavorites === 0 ? (
+      <p>No Favorites yet, start adding some?</p>
+    ) : (
+      <MeetupList meetups={favoritesCtx.favorites} />
+    );
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 };
 
 export default FavoritesPage;
