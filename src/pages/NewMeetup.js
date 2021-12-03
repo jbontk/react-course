3e9422375e99009c84c60b350f1ecb4998459210
jean <jbontikous@yahoo.fr>
2021-12-03 09:45:29 +0100

    474

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,13 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import axios from 'axios';
+
+const dbUrl =
+  'https://react-getting-started-8e716-default-rtdb.europe-west1.firebasedatabase.app/';
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (meeetupData) => {};
+  const addMeetupHandler = (meeetupData) => {
+    axios.post(`${dbUrl}/meetups.json`, meeetupData);
+  };
 
   return (
     <section>
