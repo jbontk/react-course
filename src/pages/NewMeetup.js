04df688534dce2d79f621350660a7496c424f4e3
jean <jbontikous@yahoo.fr>
2021-12-03 09:32:17 +0100

    473

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,12 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
+  const addMeetupHandler = (meeetupData) => {};
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
